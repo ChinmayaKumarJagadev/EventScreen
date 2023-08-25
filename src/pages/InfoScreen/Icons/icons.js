ba846b7f4b52ae4d3f2dd6e8e7df975e5da4606e
ChinmayaKumarJagadev <jagdev.chinmaya98@gmail.com>
2023-08-25 19:27:51 +0530

    Completed Event screen

--- src/pages/InfoScreen/Icons/icons.js
+++ src/pages/InfoScreen/Icons/icons.js
@@ -3,17 +3,21 @@ import { IMAGES } from "../../../constants/images";
 import './icons.css'
 
 const Icons = () => {
+    const iconImages = [
+        IMAGES.FaceBookIcon,
+        IMAGES.InstaIcon,
+        IMAGES.TwitterIcon,
+        IMAGES.GoogleIcon,
+        IMAGES.LinkedinIcon
+    ];
 
     return (
         <div>
             <p className="fetchText mt-3">Fetch data from</p>
-
             <div className="iconContainer mt-0">
-                <img src={IMAGES.FaceBookIcon} alt="no_image_found" width="38px" height="38px" />
-                <img src={IMAGES.InstaIcon} alt="no_image_found" width="38px" height="38px" />
-                <img src={IMAGES.TwitterIcon} alt="no_image_found" width="38px" height="38px" />
-                <img src={IMAGES.GoogleIcon} alt="no_image_found" width="38px" height="38px" />
-                <img src={IMAGES.LinkedinIcon} alt="no_image_found" width="38px" height="38px" />
+                {iconImages.map((icon, index) => (
+                    <img key={index} src={icon} alt="no_image_found" width="38px" height="38px" />
+                ))}
             </div>
 
             <p className="titleInfo mt-3">lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
